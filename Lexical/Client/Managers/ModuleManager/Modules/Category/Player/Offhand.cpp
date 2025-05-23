ec9280f5aafd82648cb6706558c7f12b7025c8ea
xuntf <xuntf@CYBERPOWER>
2025-05-22 22:04:08 -0400

    anti t fail

--- Lexical/Client/Managers/ModuleManager/Modules/Category/Player/Offhand.cpp
+++ Lexical/Client/Managers/ModuleManager/Modules/Category/Player/Offhand.cpp
@@ -10,31 +10,45 @@ std::string Offhand::getModeText() {
 }
 
 void Offhand::onNormalTick(LocalPlayer* localPlayer) {
+    if (localPlayer->getOffhandSlot()->item.get() != nullptr)
+        return;
 
-	if (localPlayer->getOffhandSlot()->item.get() != nullptr)
-		return;
+    uint16_t ItemID = (itemMode == 0) ? 584 : 362;
 
-	uint16_t ItemID = (itemMode == 0) ? 584 : 362;
+    Container* inventory = localPlayer->playerInventory->container;
+    for (int i = 0; i < 36; i++) {
+        ItemStack* itemStack = inventory->getItem(i);
+        if (itemStack == nullptr)
+            continue;
 
-	Container* inventory = localPlayer->playerInventory->container;
-	for (int i = 0; i < 36; i++) {
-		ItemStack* itemStack = inventory->getItem(i);
-		Item* item = itemStack->item.get();
-		if (item != nullptr && item->itemId == ItemID) {
-			/*
-			InventoryAction first(i, itemStack, ItemStack::getEmptyItem());
-			InventoryAction second(0, ItemStack::getEmptyItem(), itemStack, ContainerID::Offhand);
-			std::unique_ptr<ComplexInventoryTransaction> cit = std::make_unique<ComplexInventoryTransaction>();
-			cit->type = ComplexInventoryTransaction::Type::NormalTransaction;
-			cit->data.addAction(first);
-			cit->data.addAction(second);
-			InventoryTransactionPacket pk(std::move(cit));
-			localPlayer->level->getPacketSender()->send(&pk);
-			*/
-			localPlayer->setOffhandSlot(itemStack);
-			inventory->removeItem(i, 1);
-			break;
-		}
-	}
+        Item* item = itemStack->item.get();
+        if (item != nullptr && item->itemId == ItemID) {
+            InventoryAction pickUpAction(
+                i,
+                itemStack,
+                ItemStack::getEmptyItem(),
+                ContainerID::Inventory
+            );
 
+            InventoryAction placeOffhandAction(
+                0,
+                ItemStack::getEmptyItem(),
+                itemStack,
+                ContainerID::Offhand
+            );
+
+            auto cit = std::make_unique<ComplexInventoryTransaction>();
+            cit->type = ComplexInventoryTransaction::Type::NormalTransaction;
+            cit->data.addAction(pickUpAction);
+            cit->data.addAction(placeOffhandAction);
+
+            InventoryTransactionPacket packet(std::move(cit));
+            localPlayer->level->getPacketSender()->send(&packet);
+
+            localPlayer->setOffhandSlot(itemStack);
+            inventory->removeItem(i, 1);
+
+            break;
+        }
+    }
 }
